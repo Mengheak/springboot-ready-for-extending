9c8923795b0800e0c348b53a9804029011aedf29
Mengheak <mengheak088@gmail.com>
2026-09-12 20:10:17 +0700

    add default app initialization files

---
+++ src/main/java/com/chheang/mengheak/springbootsetupheakcg/SpringbootSetupHeakcgApplication.java
@@ -0,0 +1,13 @@
+package com.chheang.mengheak.springbootsetupheakcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSetupHeakcgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSetupHeakcgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chheang/mengheak/springbootsetupheakcg/SpringbootSetupHeakcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chheang.mengheak.springbootsetupheakcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSetupHeakcgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
