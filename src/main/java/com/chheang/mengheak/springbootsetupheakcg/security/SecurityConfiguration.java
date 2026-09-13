285d66592a48be48c9fe2b1026791c9cc30e88df
Mengheak <mengheak088@gmail.com>
2026-09-13 12:48:47 +0700

    feat(security): add cors config for better security

--- src/main/java/com/chheang/mengheak/springbootsetupheakcg/security/SecurityConfiguration.java
+++ src/main/java/com/chheang/mengheak/springbootsetupheakcg/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.chheang.mengheak.springbootsetupheakcg.security;
 
+import com.chheang.mengheak.springbootsetupheakcg.configs.properties.CorsProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +13,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
